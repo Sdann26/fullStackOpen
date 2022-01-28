6a719cd8a034ceceb6031d3dc9ead55017211cc3
Danilo <sdann_26@hotmail.com>
2022-01-28 04:40:36 -0500

    Exercise 1.2 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,16 +15,18 @@ const Total = (props) => {
 const Content = (props) => {
   return(
     <>
-      <p>
-        {props.part1} {props.ex1}
-      </p>
-      <p>
-        {props.part2} {props.ex2}
-      </p>
-      <p>
-        {props.part3} {props.ex3}
-      </p>
+      <Part part={props.part1} ex={props.ex1} />
+      <Part part={props.part2} ex={props.ex2} />
+      <Part part={props.part3} ex={props.ex3} />
     </>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.ex}
+    </p>
   )
 }
 
