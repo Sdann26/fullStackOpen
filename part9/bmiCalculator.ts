593b4c0e0ee50636bc8b27579cbe426accaf0c2f
Danilo Blas <sdann_26@hotmail.com>
2023-08-14 02:44:36 -0500

    Exercise 9.3 completed

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,17 +1,36 @@
+const parseArguments = (args: Array<string>): { value1: number, value2: number } => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 /**
  * @param {number} height - in cm
  * @param {number} weight - in kg
  */
 const calculateBmi = (height: number, weight: number): string => {
-  const BMI = weight / ((height / 100) * (height / 100))
-  if(BMI < 16.0) return 'Underweight (Severe thinness)'
-  else if(BMI < 16.9) return 'Underweight (Moderate thinness)'
-  else if(BMI < 18.4) return 'Underweight (Mild thinness)'
-  else if(BMI < 24.9) return 'Normal range'
-  else if(BMI < 29.9) return 'Overweight (Pre-obese)'
-  else if(BMI < 34.9) return 'Obese (Class I)'
-  else if(BMI < 39.9) return 'Obese (Class I)'
-  else return 'Obese (Class I)'
-}
+  const BMI = weight / ((height / 100) * (height / 100));
+  if (BMI < 16.0) return 'Underweight (Severe thinness)';
+  else if (BMI < 16.9) return 'Underweight (Moderate thinness)';
+  else if (BMI < 18.4) return 'Underweight (Mild thinness)';
+  else if (BMI < 24.9) return 'Normal range';
+  else if (BMI < 29.9) return 'Overweight (Pre-obese)';
+  else if (BMI < 34.9) return 'Obese (Class I)';
+  else if (BMI < 39.9) return 'Obese (Class II)';
+  else return 'Obese (Class III)';
+};
 
-console.log(calculateBmi(180, 74))
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (error) {
+  console.error(error);
+}
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -8,45 +8,62 @@ interface Result {
   average: number
 }
 
-const calculateExercises = (days: number[], target: number): Result => {
-  try {
-    if(days.some(day => day <= 0) || target <= 0) throw new Error('The hours cannot be negative')
-    const periodLength = days.length
-    const trainingDays = days.filter(day => day > 0).length
-    const average = days.reduce((previous, current) => previous += current, 0) / periodLength
-    const success = average >= target
-    let rating: 1 | 2 | 3;
+const parseArgumentsCalculator = (args: Array<string>): { value1: number[], value2: number } => {
+  if (args.length < 4) throw new Error('Not enough arguments');
 
-    if(success){
-      rating = 3
-    }
-    else {
-      if(average > target / 2){
-        rating = 2
-      }
-      else {
-        rating = 1
-      }
-    }
+  const [, , targetValueRaw, ...daysValuesRaw] = args;
 
-    const ratingTypeDescription = {
-      1: 'really bad',
-      2: 'not too bad but could be better',
-      3: 'good work you did well'
-    }
+  const daysValues: number[] = daysValuesRaw.map(arg => Number(arg));
+  const targetValue: number = Number(targetValueRaw);
 
+  if (!daysValues.some(dayValue => isNaN(dayValue)) && !isNaN(targetValue)) {
     return {
-      periodLength,
-      trainingDays,
-      success,
-      rating,
-      ratingDescription: ratingTypeDescription[rating],
-      target,
-      average
-    }
-  } catch (error) {
-    console.log(error)
+      value1: daysValues,
+      value2: targetValue
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
-}
+};
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const calculateExercises = (days: number[], target: number): Result => {
+  if (days.some(day => day < 0) || target <= 0) throw new Error('The hours cannot be negative');
+  const periodLength = days.length;
+  const trainingDays = days.filter(day => day > 0).length;
+  const average = days.reduce((previous, current) => previous += current, 0) / periodLength;
+  const success = average >= target;
+  let rating: 1 | 2 | 3;
+
+  if (success) {
+    rating = 3;
+  } else {
+    if (average > target / 2) {
+      rating = 2;
+    } else {
+      rating = 1;
+    }
+  }
+
+  const ratingTypeDescription = {
+    1: 'really bad',
+    2: 'not too bad but could be better',
+    3: 'good work you did well'
+  };
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription: ratingTypeDescription[rating],
+    target,
+    average
+  };
+};
+
+try {
+  const { value1, value2 } = parseArgumentsCalculator(process.argv);
+  console.log(calculateExercises(value1, value2));
+} catch (error) {
+  console.error(error);
+}
